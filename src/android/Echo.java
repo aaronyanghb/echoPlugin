c550687cfafa0ff67ae19fbd9968e7ba829a0b8c
aaronyanghb <aaron.yanghuibin@gmail.com>
2014-07-30 13:01:37 +0800

    echo plugins

---
+++ src/android/Echo.java
@@ -0,0 +1,34 @@
+package com.example.myPlugin;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaWebView;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+public class Echo extends CordovaPlugin{
+	@Override
+	public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+	    super.initialize(cordova, webView);
+	    // your init code here
+	}
+	
+	 @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if ("echo".equals(action)) {
+            this.echo(args.getString(0),callbackContext);
+            callbackContext.success();
+            return true;
+        }
+        return false;  // Returning false results in a "MethodNotFound" error.
+    }
+	 
+	 private void echo(String message, CallbackContext callbackContext) {
+         if (message != null && message.length() > 0) {
+             callbackContext.success("Message received: "+message);
+         } else {
+             callbackContext.error("Error occurred");
+         }
+     }
+}
---
+++ www/echo.js
@@ -0,0 +1,11 @@
+var echoPlugin = {
+    sendMessage: function(message, callback) {
+        cordova.exec(
+        	callback, // success callback function
+        	callback, // error callback function
+            'Echo', // mapped to our native Java class called "CalendarPlugin"
+            'echo', // with this action name
+            [message]
+        ); 
+     }
+}
